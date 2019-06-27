d8b94c813d1085c33522b77433e4de5e8caf0697
Nguyễn Huy <seminerd>
2019-06-27 10:04:21 +0700

    heyyyyy

---
+++ src/main/java/com/sapoyoungtalent/PhoneStoreSB/PhoneStoreSbApplication.java
@@ -0,0 +1,13 @@
+package com.sapoyoungtalent.PhoneStoreSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneStoreSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneStoreSbApplication.class, args);
+	}
+
+}
